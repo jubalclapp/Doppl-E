66dd821cef4a4ef7e7862efec5f2132a89596d26
jubalclapp <jubalclapp@gmail.com>
2026-07-04 12:10:40 -0400

    verify audio capture pipeline, confirm UGREEN ADC functions on device 2

---
+++ software/TestAudioCapture/main.py
@@ -0,0 +1,20 @@
+# Doppl-E | Audio Input Test Pipeline
+# Audio Input Test Pipeline confirms laptop is capable of receiving input signals prior to hardware integration
+# Test signal: with human touch, speaker output, connected hardware, etc
+# Author: <ME>
+import sounddevice as sd
+import numpy as np
+print(sd.query_devices()) # lists all devices on your laptop for debugging, comment at your discretion
+
+duration = 5 # duration of recording(seconds)
+sample_rate = 44100 # usb/aux sample rate (Hz)
+print("Start Recording...")
+audio = sd.rec(int(duration * sample_rate),
+               samplerate = sample_rate,
+               channels = 1,
+               device = 2) # UGREEN USB Audio Device - may vary user by user, use line 7 to verify
+sd.wait()
+print("Max amplitude:", np.max(np.abs(audio)))
+print("Done")
+
+
